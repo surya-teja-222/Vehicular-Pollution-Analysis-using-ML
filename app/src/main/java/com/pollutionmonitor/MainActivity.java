c9d87813911a247369f7e71a5c32f1a819175ce0
Surya Teja Reddy <e20cse185@bennett.edu.in>
2021-09-21 14:54:16 +0530

    splashscreen ,

--- app/src/main/java/com/pollutionmonitor/MainActivity.java
+++ app/src/main/java/com/pollutionmonitor/MainActivity.java
@@ -3,21 +3,25 @@ package com.pollutionmonitor;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Toast;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
 
         Random rnd = new Random();
         int number = rnd.nextInt(100);
@@ -39,7 +43,13 @@ public class MainActivity extends AppCompatActivity {
                     Toast.LENGTH_LONG).show();
         }
 
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this , UserDashboard.class);
+                startActivity(intent);
+            }
+        } , 5100);
 
 
 
@@ -61,6 +71,13 @@ public class MainActivity extends AppCompatActivity {
                     "Failed to connect to internet.",
 
                     Toast.LENGTH_LONG).show();
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException ignored) {
+
+            }
+            this.finishAffinity();
+
             return false;
         }
         return true;
