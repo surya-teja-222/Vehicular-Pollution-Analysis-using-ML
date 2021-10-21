d12091e8e6551773b2560d14b73154105f3f19cb
Surya Teja Reddy <e20cse185@bennett.edu.in>
2021-10-21 18:50:50 +0530

    Extensive side bar
    Signed-off-by: Surya Teja Reddy <e20cse185@bennett.edu.in>

--- app/src/main/java/com/pollutionmonitor/userDashboard.java
+++ app/src/main/java/com/pollutionmonitor/userDashboard.java
@@ -1,13 +1,20 @@
 package com.pollutionmonitor;
 
 import android.content.Intent;
+import android.graphics.drawable.DrawableWrapper;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.graphics.drawable.DrawerArrowDrawable;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +23,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -29,17 +37,21 @@ import com.google.firebase.database.ValueEventListener;
 import com.pollutionmonitor.ui.main.SectionsPagerAdapter;
 import com.pollutionmonitor.databinding.ActivityUserDashboardBinding;
 
-public class userDashboard extends AppCompatActivity {
+public class userDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private ActivityUserDashboardBinding binding;
     private FirebaseAuth firebaseAuth;
 
     private FirebaseDatabase firebaseDatabase;
 
-    TextView name ;
-    private  boolean fetched;
+    TextView name;
+    private boolean fetched;
     private DatabaseException err;
-    private  DataSnapshot result;
+    private DataSnapshot result;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    androidx.appcompat.widget.Toolbar title;
 
 
     @Override
@@ -54,31 +66,37 @@ public class userDashboard extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = binding.fab;
+
+
+        /* --------------HOOKS------------- */
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        title = findViewById(R.id.title);
+
+        setSupportActionBar(title);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, title, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
 
 
 
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser user = firebaseAuth.getCurrentUser();
-        if(user == null){
-            startActivity(new Intent(getApplicationContext() , LoginActivity.class));
+        if (user == null) {
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             finish();
         }
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
 
         System.out.println(binding.getRoot().toString());
-
-
-
-
 
 
         firebaseAuth = FirebaseAuth.getInstance();
@@ -87,20 +105,21 @@ public class userDashboard extends AppCompatActivity {
         DatabaseReference databaseReference = firebaseDatabase.getReference("Users").child(firebaseAuth.getUid());
 
 
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.main , menu);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
     }
 }
